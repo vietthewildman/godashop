f8044ea4994d0605529ac62418724a3c7c2e3923
vietthewildman <67698435+vietthewildman@users.noreply.github.com>
2022-03-16 08:49:20 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php 
+// Gán giá trị vô một hằng: dùng define
+define('SERVERNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DBNAME', 'godashop_k45');
+
+define('ITEM_PER_PAGE', 10);
+
+// SMTP (send email)
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USERNAME', 'hoangviet.nguyenphu@gmail.com');
+define('SMTP_SECRET', 'nkeuzabecfqpuomf');
+define('SHOP_OWNER', 'hoangviet.nguyenphu@gmail.com');
+
+define("GOOGLE_RECAPTCHA_SITE", "6LcpF2QdAAAAAGzSAsBN8QRCeMahVZy2k_nDMp7z");
+define("GOOGLE_RECAPTCHA_SECRET", "6LcpF2QdAAAAABtSP51aVx-Spn9lT9PPuq_DBqPr");
+
+define("JWT_KEY", "nguyenphuhoangviet");
+
+// GOOGLE LOGIN
+define("GOOGLE_CLIENT_ID", "857240243229-7bkpk5abnav1onrr257e72oao3jmjkl4.apps.googleusercontent.com");
+define("GOOGLE_CLIENT_SECRET", "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");
+
+// FACEBOOK LOGIN
+define("FACEBOOK_CLIENT_ID", "639833160366155");
+define("FACEBOOK_CLIENT_SECRET", "b97669462bf09a211f933b62069a6fd1");
+?>
